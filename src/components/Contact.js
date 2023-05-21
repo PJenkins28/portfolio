23b2e18da516b947b0673cb1a293195ca2c16492
Paige Jenkins <paigemjenkins28@gmail.com>
2023-05-21 00:09:43 -0400

    basic styling of Contact component

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -5,9 +5,14 @@ import { faArrowDown } from "@fortawesome/free-solid-svg-icons";
 export default function Contact(props) {
   return (
     <section className="contact-wrapper" ref={props.contactRef}>
+      <h2>
+        Let's work <span>together</span>
+      </h2>
       <form>
-        <input type="text" placeholder="Name"></input>
-        <input type="email" placeholder="Name"></input>
+        <section className="inputs">
+          <input type="text" placeholder="Name"></input>
+          <input type="email" placeholder="Email"></input>
+        </section>
         <textarea placeholder="Make it a good one (;"></textarea>
       </form>
     </section>
